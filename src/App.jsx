7b8f6c29eabe657d61d470a3740b24e2780b1e13
Ahmad Hassan <146438862+ahmadhassan44@users.noreply.github.com>
2025-04-30 15:25:44 +0500

    chore/remove unwanted code

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,9 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-import ProfileEditor from './components/ProfileEditor'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-    <ProfileEditor></ProfileEditor>
     </>
   )
 }
